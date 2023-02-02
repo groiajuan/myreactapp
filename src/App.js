e3c17f1e726a4cd88c3deb4c238bfa183241ca7f
Juan Groia <j.groia@makingsense.com>
2023-02-02 13:58:57 -0300

    See

--- src/App.js
+++ src/App.js
@@ -47,24 +47,6 @@ function App() {
   };
   
   return (
-<<<<<<< Updated upstream
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="http://fiufiu.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
     <React.Fragment>
       <TodoCounter
         total={totalTodos}
@@ -89,7 +71,6 @@ function App() {
 
       <CreateTodoButton />
     </React.Fragment>
->>>>>>> Stashed changes
   );
 }
 
